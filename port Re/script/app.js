58f21909f133acd86f4d517b1dc4b529954de634
Dikshit <63474879+Dikshit28@users.noreply.github.com>
2021-07-20 07:49:38 +0530

    added logs

--- port Re/script/app.js
+++ port Re/script/app.js
@@ -1,17 +1,21 @@
+console.log("fun");
 //Get the button:
 const mybutton = document.getElementById("myBtn");
 const home = document.getElementById("home");
 const nav = document.querySelector(".nav");
 const dist = home.clientHeight - nav.clientHeight;
+console.log(dist, home.clientHeight, nav.clientHeight);
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () { scrollFunction() };
 function scrollFunction() {
     if (document.body.scrollTop > dist || document.documentElement.scrollTop > dist) {
         mybutton.style.display = "block";
         nav.style.position = "absolute";
+        console.log("fun");
     } else {
         mybutton.style.display = "none";
         nav.style.position = "fixed";
+        console.log("fun");
     }
 }
 
@@ -20,3 +24,4 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+console.log("fun");
