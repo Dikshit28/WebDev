3557a4a8bc421603bfe4705a890fbab8c0c82137
Dikshit <63474879+Dikshit28@users.noreply.github.com>
2021-07-25 13:50:56 +0530

    projects dalne reh gaye

--- PorfolioMinimal/script/app.js
+++ PorfolioMinimal/script/app.js
@@ -1,10 +1,7 @@
-console.log("fun");
 //Get the button:
 const mybutton = document.getElementById("myBtn");
 const home = document.querySelector(".home");
 const nav = document.querySelector(".nav");
-const dist = home.clientHeight - nav.clientHeight;
-console.log(dist, home.clientHeight, nav.clientHeight);
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () { scrollFunction() };
 function scrollFunction() {
@@ -23,4 +20,45 @@ function scrollFunction() {
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// SLIDER
+const cards = document.querySelectorAll(".card");
+const next = document.getElementById("next");
+const prev = document.getElementById("prev");
+let i = 1;
+console.log(cards, next, prev);
+function slider(button) {
+    if (button === "next") {
+        if (i === 1) {
+            cards[2].classList.remove("viewer-r");
+            cards[3].classList.remove("viewer-l");
+            cards[0].classList.add("viewer-r");
+            cards[1].classList.add("viewer-l");
+            i = 3;
+        }
+        else {
+            cards[2].classList.add("viewer-r");
+            cards[3].classList.add("viewer-l");
+            cards[0].classList.remove("viewer-r");
+            cards[1].classList.remove("viewer-l");
+            i = 1;
+        }
+    }
+    else if (button === "prev") {
+        if (i === 3) {
+            cards[2].classList.add("viewer-r");
+            cards[3].classList.add("viewer-l");
+            cards[0].classList.remove("viewer-r");
+            cards[1].classList.remove("viewer-l");
+            i = 1;
+        }
+        else {
+            cards[2].classList.remove("viewer-r");
+            cards[3].classList.remove("viewer-l");
+            cards[0].classList.add("viewer-r");
+            cards[1].classList.add("viewer-l");
+            i = 3;
+        }
+    }
 }
---
+++ TabularPort/script/app.js
@@ -0,0 +1,7 @@
+const sections = document.querySelectorAll('.sec');
+console.log(sections);
+function switcher(classname) {
+    sections.forEach(function (section) {
+        console.log("FUN");;
+    });
+}
