c4b77f2ecd17acf0688dc23a084f3a8496bea208
Dikshit <63474879+Dikshit28@users.noreply.github.com>
2021-08-01 17:18:07 +0530

    update

--- TabularPort/script/app.js
+++ TabularPort/script/app.js
@@ -1,7 +1,23 @@
-const sections = document.querySelectorAll('.sec');
-console.log(sections);
-function switcher(classname) {
-    sections.forEach(function (section) {
-        console.log("FUN");;
+const faders = document.querySelectorAll(".fader");
+const appearOptions = {
+    threshold: 0,
+};
+
+const appearOnScroll = new IntersectionObserver(function (
+    entries,
+    appearOnScroll
+) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add("appear");
+            appearOnScroll.unobserve(entry.target);
+        }
     });
-}
+},
+    appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+});
